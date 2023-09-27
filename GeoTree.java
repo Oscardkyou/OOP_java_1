92896f6384a4e1a5ca2088554d36b4a2a1e9d94b
«Oscardk» <dauren2050@yandex.ru>
2023-09-27 15:47:30 +0600

    hw_java

---
+++ GeoTree.java
@@ -0,0 +1,41 @@
+package OOP1.JavaOOP1;
+
+import java.util.ArrayList;
+
+public class GeoTree {
+
+    private ArrayList<Node> tree = new ArrayList<>();
+
+    public ArrayList<Node> getTree() {
+        return tree;
+    
+    public ArrayList<Node> getTree() {
+        return tree;
+    }
+    }
+
+    // связь родитель - ребенок
+    public void appendPerentChild(Person parent, Person children) {
+        tree.add(new Node(parent, Relationship.parent, children));
+        tree.add(new Node(children, Relationship.children, parent));
+    
+    public ArrayList<Node> getTree() {
+        return tree;
+    }
+    }
+
+    // связь муж - жена
+    public void appendVifeHusbent(Person vife, Person husbent) {
+        tree.add(new Node(vife, Relationship.vife, husbent));
+        tree.add(new Node(husbent, Relationship.husbent, vife));
+    
+    public ArrayList<Node> getTree() {
+        return tree;
+    }
+    }
+
+
+    public ArrayList<Node> getTree() {
+        return tree;
+    }
+    }
